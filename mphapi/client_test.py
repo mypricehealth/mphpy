c307f10afcc8d65443cb32ad2a905fcd1c03e913
David Archibald <david@davidarchibald.dev>
2025-11-04 12:26:39 -0800

    Use fake credentials in mphpy

--- mphapi/client_test.py
+++ mphapi/client_test.py
@@ -9,7 +9,7 @@ import pytest
 from pytest_snapshot.plugin import Snapshot  # type: ignore
 
 from .client import Claim, Client, PriceConfig
-from .credentials import Credentials
+from .credentials import Credentials, sign_in
 from .date import DateTime
 from .env import load_env
 from .pricing import ClaimStatus, PricedService, Pricing, status_new
@@ -41,7 +41,9 @@ def test_client(snapshot: Snapshot):
         referer=app_referer,
         credentials_path=Path("fake-credentials-path"),
         email="test-user@mypricehealth.com",
-        id_token="fake-id-token",
+        # An id token with some minimal user info.
+        # Will not work if tested against a service that expects a real id token.
+        id_token="eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImFhYSI6dHJ1ZX0.eyJpc3MiOiJ0ZXN0IGlzc3VlciIsImVtYWlsIjoidGVzdC11c2VyQG15cHJpY2VoZWFsdGguY29tIiwic3ViIjoidGVzdCBzdWJqZWN0IiwiYXVkIjoidGVzdCBhdWRpZW5jZSIsImV4cCI6MCwiaWF0IjowLCJpZCI6IjEyMzQ1Njc4OTAiLCJyb2xlcyI6WyJBZG1pbiJdfQ.iCx0YI9L9qo7KCkIq1w58C0mBdiCDhikPZsM3Mx78j11Ln3WxWpAnm0IomIRfrDVU1JczW8OjSj5os7igp9fYufZwObLV5N5eDd9LmYtXs2EcO8EFHcM7HeHx6lnkT1GxX-4J_946WbIdHYnpLyoXLZF_MZNfEwsN1UBG6YdaRcwfhF9vJHt6YM7-IoOcHvdLiEWv06Y9eC0v--_R_x8GwjSrE0Z9EyZw3hMz94QZ5VgUf8e89NexeVGIoD4pUOHuYmNIx1ca_UTIOg81exhhnh4g190jUSer5582YJc5Hx7gts3DyizRmAK8Glcwhnc7WKvZDQaSjtbHzIARnUKtw",
         refresh_token="fake-refresh-token",
         expires_at=sys.float_info.max,
     )
@@ -95,3 +97,21 @@ def test_client(snapshot: Snapshot):
         )
 
         pass
+
+
+def test_signin():
+    app_api_key = os.getenv("FIREBASE_API_KEY")
+    if app_api_key is None:
+        raise Exception("FIREBASE_API_KEY must be set")
+
+    test_user = os.getenv("FIREBASE_TEST_USER")
+    if test_user is None:
+        pytest.skip("Skipping because FIREBASE_TEST_USER is not set")
+
+    test_password = os.getenv("FIREBASE_TEST_PASSWORD")
+    if test_password is None:
+        pytest.skip("Skipping because FIREBASE_TEST_PASSWORD is not set")
+
+    credentials = sign_in(app_api_key, test_user, test_password)
+
+    assert credentials.email == test_user
